528326bd83967acbbee4169e91cc070a38fb5cbe
sarisen <serafettin@turkayazilim.com>
2026-03-24 07:32:05 +0300

    Reduce local draft save frequency

--- CloakNote/Utilities/Constants.swift
+++ CloakNote/Utilities/Constants.swift
@@ -7,7 +7,7 @@ enum Constants {
     static let pbkdf2Iterations = 600_000
     static let saltLength = 32
     static let defaultRepoName = "cloaknote-notes"
-    static let localDraftSaveDelay: TimeInterval = 0.5
+    static let localDraftSaveDelay: TimeInterval = 5
     static let defaultSyncInterval: TimeInterval = 300
     static let minPassphraseLength = 16
 }
